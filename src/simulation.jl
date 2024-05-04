923ee6354bfea08d9b8478d5ba346577d82053d4
Frederick Groth <fgroth@usm.lmu.de>
2024-05-04 20:16:10 +0200

    add some more functions to deal with simulations more easily

--- src/simulation.jl
+++ src/simulation.jl
@@ -6,19 +6,14 @@ using Formatting
 Get the name of the last snapshot. 
 """
 function get_last_snapshot(simulation_dir::String="./"; include_directory::Bool=false)
-    all_files = readdir(simulation_dir)
-    snaps=all_files[contains.(all_files,"snap_") .|| contains.(all_files,"snapdir_")]
+    snaps = get_all_snapshots(GadgetSimulationDir(simulation_dir))
     last_snap = sort(snaps)[end]
     dir = if include_directory
         simulation_dir
     else
         ""
     end
-    if contains(last_snap,"snapdir_")
-        return dir*last_snap*"/snap_"*last_snap[end-2:end]
-    else
-        return dir*last_snap
-    end
+    return dir*last_snap
 end
 
 """
@@ -40,8 +35,7 @@ Return snapshot with desired number `i_snap`. If this does not exist, return the
 function get_snapshot(simulation_dir::String="./"; include_directory::Bool=false,
                       i_snap::Int64=0,
                       first_snap_to_consider::Int64=0)
-    all_files = readdir(simulation_dir)
-    snaps = all_files[contains.(all_files,"snap_") .|| contains.(all_files,"snapdir_")]
+    snaps = get_all_snapshots(GadgetSimulationDir(simulation_dir))
     dir = if include_directory
         simulation_dir
     else
@@ -54,4 +48,101 @@ function get_snapshot(simulation_dir::String="./"; include_directory::Bool=false
             return dir*"snapdir_"*sprintf1("%03d",this_i_snap)*"/snap_"*sprintf1("%03d",this_i_snap)
         end
     end
+end
+
+"""
+    issnap(file::String)
+
+Return if a file or directory is a snapshot according to its name. 
+"""
+function issnap(file::String)
+    return startswith(file,"snap_") || startswith(file,"snapdir_")
+end
+
+"""
+    issub(file::String)
+
+Return if a file or directory is a subfind output according to its name. 
+"""
+function issub(file::String)
+    return startswith(file,"sub_") || startswith(file,"groups_")
+end
+
+"""
+    get_all_snapshots(simulation::GadgetSimulationDir)
+
+Return all snapshots for give `simulation`.
+"""
+function get_all_snapshots(simulation::GadgetSimulationDir)
+    all_files = readdir(simulation.dir)
+    snaps = all_files(issnap.(all_files))
+    # now add the file instead of the directory if snaps are inside directorires
+    for i_snap in 1:length(snaps)
+        if startswith(snaps[i_snap],"snapdir_")
+            snaps[i_snap] = snaps[i_snap]*"/snap_"*snaps[i_snap][end-3:end]
+        end
+    end
+    return snaps
+end
+
+"""
+    get_all_subs(simulation::GadgetSimulationDir)
+
+Return all subfind outputs for given `simulation`.
+"""
+function get_all_subs(simulation::GadgetSimulationDir)
+    all_files = readdir(simulation.dir)
+    subs = all_files(issub.(all_files))
+    # now add the file instead of the directory if subs are inside directorires
+    for i_sub in 1:length(sub)
+        if startswith(subs[i_sub],"groups_")
+            subs[i_sub] = subs[i_sub]*"/sub_"*snaps[i_sub][end-3:end]
+        end
+    end
+    return subs
+end
+
+"""
+    get_previous_snapshot(i_snap::Int64, simulation::GadgetSimulationDir)
+
+Get name of previous snapshot before current number.
+"""
+function get_previous_snapshot(i_snap::Int64, simulation::GadgetSimulationDir)
+    all_snaps = get_all_snapshots(simulation)
+    for previous_snap in i_snap-1:-1:0
+        if "snap_"*sprintf1("%03d",previous_snap) in all_snaps
+            "snap_"*sprintf1("%03d",previous_snap)
+        elseif "snapdir_"*sprintf1("%03d",previous_snap)*"/snap_"*sprintf1("%03d",previous_snap) in all_snaps
+            "snapdir_"*sprintf1("%03d",previous_snap)*"/snap_"*sprintf1("%03d",previous_snap)
+        end
+    end
+    # if we reach this point, there is no further snapshot in simulation
+end
+
+"""
+    get_next_snapshot(i_snap::In64, simulation::GadgetSimulationDir)
+
+Get name of next snapshot after current number.
+"""
+function get_next_snapshot(i_snap::In64, simulation::GadgetSimulationDir)
+    for next_snap in i_snap+1:0
+        if "snap_"*sprintf1("%03d",next_snap) in all_snaps
+            "snap_"*sprintf1("%03d",next_snap)
+        elseif "snapdir_"*sprintf1("%03d",next_snap)*"/snap_"*sprintf1("%03d",next_snap) in all_snaps
+            "snapdir_"*sprintf1("%03d",next_snap)*"/snap_"*sprintf1("%03d",next_snap)
+        end
+
+    end
+    # if we reach this point, there is no further snapshot in simulation
+end
+
+"""
+    get_snapshot_closest_to_redshift(simulation::GadgetSimulationDir, redshift::Number)
+
+todo
+"""
+function get_snapshot_closest_to_redshift(simulation::GadgetSimulationDir, redshift::Number)
+    relevant = argmin(abs.(all_redshift[i_cluster,:] .- comparison_redshift))
+    # maybe start reading at first and last, then iterate.
+    # todo!
 end
