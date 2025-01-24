2388377bdc9bfb09de75ba166600188bbde7d00a
Frederick Groth <fgroth@usm.lmu.de>
2025-01-24 09:33:13 +0100

    some cleanup: remove constructors that take data directly, always first construct without data. add selection function and select_particle_types data members, which can be used later to filter data when reading.

--- src/gadget_data_structure.jl
+++ src/gadget_data_structure.jl
@@ -8,12 +8,32 @@ Datatype to hold Gadget `snap` and `sub` filenames.
 struct GadgetFilename <: GadgetData
     snap::String
     sub::String
+    selection_function::Function
+    select_particle_types::BitVector
     
-    function GadgetFilename(snap::String, sub::String)
-        new(snap,sub)
+    function GadgetFilename(snap::String, sub::String;
+                            selection_function::Function=(i->true), select_particle_types::Union{Nothing,BitVector}=nothing)
+        new(snap,sub,
+            selection_function,get_select_particle_types(select_particle_types))
     end
-    function GadgetFilename(snap::String)
-        new(snap,snap2sub(snap))
+    function GadgetFilename(snap::String; kwargs...)
+        GadgetFilename(snap,snap2sub(snap); kwargs...)
+    end
+end
+
+"""
+    get_select_particle_types(select_particle_types::Union{Nothing,BitVector})
+
+Return default select_particle_types for `nothing` and otherwise check the length to be 6.
+"""
+function get_select_particle_types(select_particle_types::Union{Nothing,BitVector})
+    if select_particle_types == nothing
+        select_particle_types = BitVector(undef,6)
+        select_particle_types .= false
+    else
+        if length(select_particle_types != 6)
+            error("Length of select_particle_type must be 6.")
+        end
     end
 end
 
@@ -27,37 +47,26 @@ mutable struct GadgetFilenameWithData <: GadgetData
     sub::String
     snap_data::Dict
     sub_data::Dict
+    selection_function::Function
+    select_particle_types::BitVector
     """
-        GadgetFilenameWithData(snap::String,sub::String, snap_data::Dict,sub_data::Dict)
+        GadgetFilenameWithData(snap::String,sub::String)
 
-    Construct a `GadgetFilenameWithData`.
+    Construct a `GadgetFilenameWithData` with un-initialzed `snap_data` and `sub_data`.
     """
-    function GadgetFilenameWithData(snap::String,sub::String, snap_data::Dict,sub_data::Dict)
-        new(snap,sub, snap_data,sub_data)
-    end
-    """
-        GadgetFilenameWithData(snap::String, snap_data::Dict,sub_data::Dict)
-
-    Construct a `GadgetFilenameWithData`. Obtain `sub` name from `snap`.
-    """
-    function GadgetFilenameWithData(snap::String, snap_data::Dict,sub_data::Dict)
-        new(snap,snap2sub(snap), snap_data,sub_data)
-    end
-    """
-        GadgetFilenameWithData(snap::String, sub::String)
-
-    Construct a `GadgetFileWithData` with un-initialzed `snap_data` and `sub_data`.
-    """
-    function GadgetFilenameWithData(snap::String, sub::String)
-        new(snap,sub, Dict(),Dict())
+    function GadgetFilenameWithData(snap::String,sub::String;
+                                    selection_function::Function=(i->true), select_particle_types::Union{Nothing,BitVector}=nothing)
+        new(snap,sub,
+            Dict(),Dict(),
+            selection_function, get_select_particle_types(select_particle_types))
     end
     """
         GadgetFilenameWithData(snap::String)
 
-    Construct a `GadgetFileWithData` with un-initialzed `snap_data` and `sub_data`. Obtain `sub` name from `snap`.
+    Construct a `GadgetFilenameWithData`. Obtain `sub` name from `snap`.
     """
-    function GadgetFilenameWithData(snap::String)
-        new(snap,snap2sub(snap), Dict(),Dict())
+    function GadgetFilenameWithData(snap::String; kwargs...)
+        GadgetFilenameWithData(snap,snap2sub(snap); kwargs...)
     end
     
 end
@@ -70,12 +79,15 @@ Datatype to hold `snap_data` and `sub_data`.
 mutable struct GadgetOnlyData <: GadgetData
     snap_data::Dict
     sub_data::Dict
+    selection_function::Function
+    select_particle_types::BitVector
     """
         GadgetOnlyData(snap_data::Dict, sub_data::Dict)
 
-    Construct `GadgetOnlyData` with `snap_data` and `sub_data`.
+    Construct `GadgetOnlyData` with  with un-initialzed `snap_data` and `sub_data`.
     """
-    function GadgetOnlyData(snap_data::Dict, sub_data::Dict)
-        new(snap_data,sub_data)
+    function GadgetOnlyData(; selection_function::Function=(i->true), select_particle_types::Union{Nothing,BitVector}=nothing)
+        new(Dict(), Dict(),
+            selection_function, get_select_particle_types(select_particle_types))
     end
 end
