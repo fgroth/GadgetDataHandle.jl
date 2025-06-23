768469499fe2de06455795efc1fd052f8aae992d
Frederick Groth <fgroth@usm.lmu.de>
2025-06-23 16:36:44 +0200

    add doxystring

--- src/gadget_general.jl
+++ src/gadget_general.jl
@@ -1,3 +1,8 @@
+"""
+    snap2sub(snap::String)
+
+Return the corresponding sub (groups file) name for given `snap` (snapshot file) name.
+"""
 function snap2sub(snap::String)
     sub = replace(replace(snap,"snapdir"=>"groups"),"snap"=>"sub")
 end
