316cb5e12f6ebf44688c3ab3993e0f2dc1cf3e87
Frederick Groth <fgroth@usm.lmu.de>
2025-07-21 19:52:52 +0200

    cleanup alternative block names, avoid code repetetion and 'try'

--- src/sub_reader.jl
+++ src/sub_reader.jl
@@ -1,5 +1,27 @@
 using GadgetIO
 
+"""
+    choose_subfind_fieldname(data::GadgetData, fieldname::String)
+
+Return the correct choice of the fieldname, either original value, or alternative name if applicable.
+"""
+function choose_subfind_fieldname(data::GadgetData, fieldname::String)
+    alternative_fieldname = Dict("R200" => "RMEA", "RMEA"=>"R200",
+                                 "M200" => "MMEA", "MMEA"=>"M200",
+                                 "RTOP" => "RVIR", "RVIR"=>"RTOP",
+                                 "MTOP" => "MVIR", "MVIR"=>"MTOP"
+                                 )
+    if has_sub_block(data, fieldname)
+        # the field is present
+        return fieldname
+    elseif has_sub_block(data, alternative_fieldname[fieldname])
+        return alternative_fieldname[fieldname]
+    else
+        # neither the field, nor the alternative name is present.
+        return fieldname
+    end
+end
+
 # subfind reading functions
 """
     get_sub_data(data::GadgetFilename, fieldname::String)
@@ -7,29 +29,7 @@ using GadgetIO
 Read subfind data from `data.sub` with `fieldname`.
 """
 function get_sub_data(data::GadgetFilename, fieldname::String)
-    try
-        return read_subfind(data.sub, fieldname)
-    catch
-        if fieldname == "R200"
-            return get_sub_data(data,"RMEA")
-        elseif fieldname == "RMEA"
-            return get_sub_data(data,"R200")
-        elseif fieldname == "M200"
-            return get_sub_data(data,"MMEA")
-        elseif fieldname == "MMEA"
-            return get_sub_data(data,"M200")
-        elseif fieldname == "MTOP"
-            return get_sub_data(data,"MVIR")
-        elseif fieldname == "MVIR"
-            return get_sub_data(data,"MTOP")
-        elseif fieldname == "RTOP"
-            return get_sub_data(data,"RVIR")            
-        elseif fieldname == "RVIR"
-            return get_sub_data(data,"RTOP")
-        else
-            error("Block "*fieldname*" not present!")
-        end
-    end
+    read_subfind(data.sub, choose_subfind_fieldname(data, fieldname))
 end
 """
     get_sub_data(data::GadgetFilenameWithData, fieldname::String)
@@ -37,32 +37,11 @@ end
 Get subfind data from `data.sub_data` if present, otherwise read from `data.sub`.
 """
 function get_sub_data(data::GadgetFilenameWithData, fieldname::String)
-    try
-        if haskey(data.sub_data,fieldname)
-            return data.sub_data[fieldname]
-        else
-            return read_subfind(data.sub, fieldname)
-        end
-    catch
-        if fieldname == "R200"
-            return get_sub_data(data,"RMEA")
-        elseif fieldname == "RMEA"
-            return get_sub_data(data,"R200")
-        elseif fieldname == "M200"
-            return get_sub_data(data,"MMEA")
-        elseif fieldname == "MMEA"
-            return get_sub_data(data,"M200")
-        elseif fieldname == "MTOP"
-            return get_sub_data(data,"MVIR")
-        elseif fieldname == "MVIR"
-            return get_sub_data(data,"MTOP")
-        elseif fieldname == "RTOP"
-            return get_sub_data(data,"RVIR")            
-        elseif fieldname == "RVIR"
-            return get_sub_data(data,"RTOP")
-        else
-            error("Block "*fieldname*" not present!")
-        end
+    this_fieldname = choose_subfind_fieldname(data, fieldname)
+    if haskey(data.sub_data,this_fieldname)
+        return data.sub_data[this_fieldname]
+    else
+        return read_subfind(data.sub, this_fieldname)
     end
 end
 """
@@ -71,29 +50,7 @@ end
 Get subfind data from `data.sub_data`.
 """
 function get_sub_data(data::GadgetOnlyData, fieldname::String)
-    try
-        return data.sub_data[fieldname]
-    catch
-        if fieldname == "R200"
-            return get_sub_data(data,"RMEA")
-        elseif fieldname == "RMEA"
-            return get_sub_data(data,"R200")
-        elseif fieldname == "M200"
-            return get_sub_data(data,"MMEA")
-        elseif fieldname == "MMEA"
-            return get_sub_data(data,"M200")
-        elseif fieldname == "MTOP"
-            return get_sub_data(data,"MVIR")
-        elseif fieldname == "MVIR"
-            return get_sub_data(data,"MTOP")
-        elseif fieldname == "RTOP"
-            return get_sub_data(data,"RVIR")            
-        elseif fieldname == "RVIR"
-            return get_sub_data(data,"RTOP")
-        else
-            error("Block "*fieldname*" not present!")
-        end
-    end
+    return data.sub_data[choose_subfind_fieldname(data, fieldname)]
 end
 
 # subfind reading functions to add the data just read
@@ -103,29 +60,7 @@ end
 See `get_sub_data(data::GadgetData, fieldname::String)`
 """
 function get_sub_data!(data::GadgetData, fieldname::String)
-    try
-        return get_sub_data(data, fieldname)
-    catch
-        if fieldname == "R200"
-            return get_sub_data(data,"RMEA")
-        elseif fieldname == "RMEA"
-            return get_sub_data(data,"R200")
-        elseif fieldname == "M200"
-            return get_sub_data(data,"MMEA")
-        elseif fieldname == "MMEA"
-            return get_sub_data(data,"M200")
-        elseif fieldname == "MTOP"
-            return get_sub_data(data,"MVIR")
-        elseif fieldname == "MVIR"
-            return get_sub_data(data,"MTOP")
-        elseif fieldname == "RTOP"
-            return get_sub_data(data,"RVIR")            
-        elseif fieldname == "RVIR"
-            return get_sub_data(data,"RTOP")
-        else
-            error("Block "*fieldname*" not present!")
-        end
-    end
+    return get_sub_data(data, choose_subfind_fieldname(data, fieldname))
 end
 """
     get_sub_data(data::GadgetFilenameWithData, fieldname::String
@@ -133,34 +68,12 @@ end
 Get subfind data from `data.sub_data` if present, otherwise read from `data.sub` and save in `data.sub_data`.
 """
 function get_sub_data!(data::GadgetFilenameWithData, fieldname::String)
-    try
-        if haskey(data.sub_data,fieldname)
-            return data.sub_data[fieldname]
-        else
-            new_sub_data = read_subfind(data.sub, fieldname)
-            data.sub_data[fieldname] = new_sub_data
-            return new_sub_data
-        end
-    catch
-        if fieldname == "R200"
-            return get_sub_data(data,"RMEA")
-        elseif fieldname == "RMEA"
-            return get_sub_data(data,"R200")
-        elseif fieldname == "M200"
-            return get_sub_data(data,"MMEA")
-        elseif fieldname == "MMEA"
-            return get_sub_data(data,"M200")
-        elseif fieldname == "MTOP"
-            return get_sub_data(data,"MVIR")
-        elseif fieldname == "MVIR"
-            return get_sub_data(data,"MTOP")
-        elseif fieldname == "RTOP"
-            return get_sub_data(data,"RVIR")            
-        elseif fieldname == "RVIR"
-            return get_sub_data(data,"RTOP")
-        else
-            error("Block "*fieldname*" not present!")
-        end
+    this_fieldname = choose_subfind_fieldname(data, fieldname)
+    if haskey(data.sub_data,this_fieldname)
+        return data.sub_data[this_fieldname]
+    else
+        new_sub_data = read_subfind(data.sub, this_fieldname)
+        data.sub_data[this_fieldname] = new_sub_data
+        return new_sub_data
     end
 end
-
