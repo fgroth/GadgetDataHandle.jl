751a9e0b9e1dbb6bd6eb8d872a906f36e128991c
Frederick Groth <fgroth@usm.lmu.de>
2025-12-17 13:47:01 +0100

    add the BHGROWTH information as well, fix typo in ENERGY lines (was writing to wrong name before)

--- src/diagnostic_files/read_bh.jl
+++ src/diagnostic_files/read_bh.jl
@@ -50,7 +50,7 @@ function read_bh_details(directory::String="blackhole_details/")
                     bh = get!(bh_data, id) do
                         Dict{String, Any}()
                     end
-                    entry = get!(bh, "BHGROWTH") do
+                    entry = get!(bh, "ENERGY") do
                         (time = Float64[],
                          mass = Float64[],
                          mdot = Float64[],
@@ -69,7 +69,54 @@ function read_bh_details(directory::String="blackhole_details/")
                     push!(entry.total_energy, Parsers.parse(Float64, fields[14]))
                 elseif fields[1] == "BHGROWTH"
                     # accretion rate computation
-                    # todo
+                    id = parse(Int64, fields[3][6:end])
+                    bh = get!(bh_data, id) do
+                        Dict{String, Any}()
+                    end
+                    entry = get!(bh, "BHGROWTH") do
+                        (pos = Vector{NTuple{3, Float64}}(),
+                         time = Float64[],
+                         mass = Float64[],
+                         rho = Float64[],
+                         mdot_cold = Float64[],
+                         rho_cold = Float64[],
+                         csnd_cold = Float64[],
+                         bhvel_cold = Float64[],
+                         mdot_hot = Float64[],
+                         rho_hot = Float64[],
+                         csnd_hot = Float64[],
+                         bhvel_hot = Float64[],
+                         mdot_edd = Float64[],
+                         eps_tot_feed = Float64[],
+                         eps_tot = Float64[],
+                         dt = Float64[],
+                         mdot = Float64[],
+                         mass_now = Float64[],
+                         )
+                    end
+                    push!(entry.pos,        (Parsers.parse(Float64, fields[4][2:end]),
+                                             Parsers.parse(Float64, fields[5]),
+                                             Parsers.parse(Float64, fields[6][1:end-1])))
+                    push!(entry.time,        Parsers.parse(Float64, fields[7][6:end]))
+                    push!(entry.mass,        Parsers.parse(Float64, fields[8][5:end]))
+                    push!(entry.rho,         Parsers.parse(Float64, fields[10][5:end]))
+
+                    push!(entry.mdot_cold,   Parsers.parse(Float64, fields[14][6:end]))
+                    push!(entry.rho_cold,    Parsers.parse(Float64, fields[15][5:end]))
+                    push!(entry.csnd_cold,   Parsers.parse(Float64, fields[16][6:end]))
+                    push!(entry.bhvel_cold,  Parsers.parse(Float64, fields[17][7:end]))
+
+                    push!(entry.mdot_hot,    Parsers.parse(Float64, fields[22][6:end]))
+                    push!(entry.rho_hot,     Parsers.parse(Float64, fields[23][5:end]))
+                    push!(entry.csnd_hot,    Parsers.parse(Float64, fields[24][6:end]))
+                    push!(entry.bhvel_hot,   Parsers.parse(Float64, fields[25][7:end]))
+
+                    push!(entry.mdot_edd,    Parsers.parse(Float64, fields[27][9:end]))
+                    push!(entry.eps_tot_feed,Parsers.parse(Float64, fields[28][12:end]))
+                    push!(entry.eps_tot,     Parsers.parse(Float64, fields[29][8:end]))
+                    push!(entry.dt,          Parsers.parse(Float64, fields[30][4:end]))
+                    push!(entry.mdot,        Parsers.parse(Float64, fields[31][6:end]))
+                    push!(entry.mass_now,    Parsers.parse(Float64, fields[32][6:end]))
                 elseif fields[1] == "SWALLOW"
                     # gas particles accreted, todo
                 elseif fields[1] == "FRICTION"
