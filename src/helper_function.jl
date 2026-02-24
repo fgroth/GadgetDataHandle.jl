9f1b0368f07000dfb434933068b1a2ad993bdfa8
Frederick Groth <fgroth@usm.lmu.de>
2026-02-24 09:21:48 +0100

    add key index file to check

--- src/helper_function.jl
+++ src/helper_function.jl
@@ -5,7 +5,7 @@
 Return if key files exist for given `data.snap`.
 """
 function has_key_files(data::GadgetData)
-    return isfile(data.snap*".0.key")
+    return isfile(data.snap*".key.index") && isfile(data.snap*".0.key")
 end
 function has_key_files(data::GadgetOnlyData)
     return false
